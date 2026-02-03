b138c4cec514f5d748a8fa8cc9d9c009496dfae8
lequangdung1507 <lequangdung1507@gmail.com>
2026-02-03 16:27:43 +0700

    nhập 2 số

---
+++ baith2/src/tonghaiso.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class tonghaiso {
+
+	public static void main(String[] args) {
+       //	khai báo
+		double num1, num2, sum;
+		
+		//khai báo đối tượng scanner
+		Scanner banphim = new Scanner(System.in);
+		
+		// in ra thông tin, mời User nhập số thứ nhất
+		System.out.print("mời nhập số thứ nhất: ");
+		num1=banphim.nextDouble();
+		
+		System.out.print("mời nhập số thứ hai: ");
+		num2=banphim.nextDouble();
+		
+				
+		
+
+	}
+
+}
