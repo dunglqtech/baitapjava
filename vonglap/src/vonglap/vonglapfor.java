9ac8432c1886dd35528e3f1dabbcf77583781e6c
lequangdung1507 <lequangdung1507@gmail.com>
2026-02-06 10:19:33 +0700

    hoan thien code

--- vonglap/src/vonglap/vonglapfor.java
+++ vonglap/src/vonglap/vonglapfor.java
@@ -1,9 +1,17 @@
 package vonglap;
 
+import java.util.Scanner;
+
 public class vonglapfor {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner vonglap = new Scanner(System.in);
+		
+		System.out.print("nhap so lan lap: ");
+		int n = vonglap.nextInt();
+		for (int i = 0; i< n; i++) {
+			System.out.println("Hello 66CNTT2");
+		}
 
 	}
 
