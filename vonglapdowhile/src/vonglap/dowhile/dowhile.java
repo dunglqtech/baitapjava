749baec66cd3f21f672c98e9b038acc75ffbf7c0
lequangdung1507 <lequangdung1507@gmail.com>
2026-02-07 20:35:53 +0700

    hoàn thành code while do

--- vonglapdowhile/src/vonglap/dowhile/dowhile.java
+++ vonglapdowhile/src/vonglap/dowhile/dowhile.java
@@ -1,9 +1,20 @@
 package vonglap.dowhile;
 
+import java.util.Scanner;
+
 public class dowhile {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner vonglap = new Scanner (System.in);
+		System.out.print("nhap số lần lặp: ");
+		int n = vonglap.nextInt();
+		
+		int i = 1;
+		do {
+			System.out.println("Hello 66CNTT-2");
+			i++;
+		} while  (i<=n);
 
 	}
 
